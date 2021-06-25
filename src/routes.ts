581edca62e91bc07fb9d18ae22379a20fddb1233
Paulo Marcos <paulo_marcos13@hotmail.com>
2021-06-25 09:45:43 -0300

    feat: inclusão da rota users

--- src/routes.ts
+++ src/routes.ts
@@ -8,6 +8,7 @@ import { ensureAuthenticated } from "./middlewares/ensureAuthenticated";
 import { ListUserSendComplimentsController } from "./controllers/ListUserSendCOmplimentsController";
 import { ListUserReceiveComplimentsController } from "./controllers/ListUserReceiveComplimentsController";
 import { ListTagsController } from "./controllers/ListTagsController";
+import { ListUserController } from "./controllers/ListUserController";
 
 const router = Router()
 
@@ -18,6 +19,7 @@ const creatComplimentController = new CreateComplimentController()
 const listUserReceiveComplimentsController = new ListUserReceiveComplimentsController()
 const listUserSendComplimentsController = new ListUserSendComplimentsController()
 const listTagsController = new ListTagsController()
+const listUserController = new ListUserController()
 
 router.post("/users", createUserController.handle)
 router.post("/tags", ensureAuthenticated, ensureAdmin, createTagController.handle)
@@ -25,6 +27,7 @@ router.get("/tags", ensureAuthenticated, listTagsController.handle)
 router.post("/login", authenticateUserController.handle)
 router.post("/compliment", ensureAuthenticated, creatComplimentController.handle)
 
+router.get("/users", ensureAuthenticated, listUserController.handle)
 router.get("/users/compliments/send", ensureAuthenticated, listUserSendComplimentsController.handle)
 router.get("/users/compliments/receive", ensureAuthenticated, listUserReceiveComplimentsController.handle)
 
