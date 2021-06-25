9c82a83e67b5b7df5e175ac7f3828fbd7ea076d9
Paulo Marcos <paulo_marcos13@hotmail.com>
2021-06-25 09:24:21 -0300

    feat: implementação das rotas para gerar lista dos elogios

--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,8 @@ import { ensureAdmin } from "./middlewares/ensureAdmin";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateComplimentController } from "./controllers/CreateComplimentController";
 import { ensureAuthenticated } from "./middlewares/ensureAuthenticated";
+import { ListUserSendComplimentsController } from "./controllers/ListUserSendCOmplimentsController";
+import { ListUserReceiveComplimentsController } from "./controllers/ListUserReceiveComplimentsController";
 
 const router = Router()
 
@@ -12,10 +14,15 @@ const createUserController = new CreateUserController()
 const createTagController = new CreateTagController()
 const authenticateUserController = new AuthenticateUserController()
 const creatComplimentController = new CreateComplimentController()
+const listUserReceiveComplimentsController = new ListUserReceiveComplimentsController()
+const listUserSendComplimentsController = new ListUserSendComplimentsController()
 
 router.post("/users", createUserController.handle)
 router.post("/tags", ensureAuthenticated, ensureAdmin, createTagController.handle)
 router.post("/login", authenticateUserController.handle)
 router.post("/compliment", ensureAuthenticated, creatComplimentController.handle)
+
+router.get("/users/compliments/send", ensureAuthenticated, listUserSendComplimentsController.handle)
+router.get("/users/compliments/receive", ensureAuthenticated, listUserReceiveComplimentsController.handle)
 
 export { router }
