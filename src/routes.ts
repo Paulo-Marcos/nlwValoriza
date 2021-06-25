813a8450c247ccdaf21ba8026b22e5b67a977fd3
Paulo Marcos <paulo_marcos13@hotmail.com>
2021-06-25 08:59:49 -0300

    feat: implementado verificação de autenticação

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ import { CreateTagController } from "./controllers/CreateTagController";
 import { ensureAdmin } from "./middlewares/ensureAdmin";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateComplimentController } from "./controllers/CreateComplimentController";
+import { ensureAuthenticated } from "./middlewares/ensureAuthenticated";
 
 const router = Router()
 
@@ -13,8 +14,8 @@ const authenticateUserController = new AuthenticateUserController()
 const creatComplimentController = new CreateComplimentController()
 
 router.post("/users", createUserController.handle)
-router.post("/tags", ensureAdmin, createTagController.handle)
+router.post("/tags", ensureAuthenticated, ensureAdmin, createTagController.handle)
 router.post("/login", authenticateUserController.handle)
-router.post("/compliment", creatComplimentController.handle)
+router.post("/compliment", ensureAuthenticated, creatComplimentController.handle)
 
 export { router }
