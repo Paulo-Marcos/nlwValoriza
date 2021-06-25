b852ea86d923b55c61cf4a96ecd6e7f949188c60
Paulo Marcos <paulo_marcos13@hotmail.com>
2021-06-25 09:33:59 -0300

    feat: inclusão da rota GET/tags

--- src/routes.ts
+++ src/routes.ts
@@ -7,6 +7,7 @@ import { CreateComplimentController } from "./controllers/CreateComplimentContro
 import { ensureAuthenticated } from "./middlewares/ensureAuthenticated";
 import { ListUserSendComplimentsController } from "./controllers/ListUserSendCOmplimentsController";
 import { ListUserReceiveComplimentsController } from "./controllers/ListUserReceiveComplimentsController";
+import { ListTagsController } from "./controllers/ListTagsController";
 
 const router = Router()
 
@@ -16,9 +17,11 @@ const authenticateUserController = new AuthenticateUserController()
 const creatComplimentController = new CreateComplimentController()
 const listUserReceiveComplimentsController = new ListUserReceiveComplimentsController()
 const listUserSendComplimentsController = new ListUserSendComplimentsController()
+const listTagsController = new ListTagsController()
 
 router.post("/users", createUserController.handle)
 router.post("/tags", ensureAuthenticated, ensureAdmin, createTagController.handle)
+router.get("/tags", ensureAuthenticated, listTagsController.handle)
 router.post("/login", authenticateUserController.handle)
 router.post("/compliment", ensureAuthenticated, creatComplimentController.handle)
 
